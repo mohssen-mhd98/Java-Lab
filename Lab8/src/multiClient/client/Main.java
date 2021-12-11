56aeafbd5ee0112845f98ff7b532d1aff7248b87
mohsen mohammadian <62768892+mohssen-mhd98@users.noreply.github.com>
2021-12-11 13:40:34 +0330

    Add files via upload

---
+++ Lab8/src/multiClient/client/Main.java
@@ -0,0 +1,36 @@
+package multiClient.client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try (Socket client = new Socket("127.0.0.1", 5000)) {
+            System.out.println("Connected to server.");
+            OutputStream out = client.getOutputStream();
+            InputStream in = client.getInputStream();
+            byte[] buffer = new byte[2048];
+            String massage = sc.nextLine();
+            while (true) {
+                out.write(massage.getBytes());
+                System.out.println("SENT: " + massage);
+                int read = in.read(buffer);
+                if(read==-1){
+                    break;
+                }
+                System.out.println("RECV: " + new String(buffer, 0, read));
+                massage = sc.nextLine();
+            }
+            System.out.print("All messages sent.\nClosing ... ");
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+        System.out.println("done.");
+
+    }
+}
